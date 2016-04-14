7d474435e1647aa741ca57294cccb69aadbcf70d
beifengpiaoxue <345167261@qq.com>
2016-04-14 10:53:53 +0800

    添加baseActivity

---
+++ app/src/main/java/frame/example/com/baseframe/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package frame.example.com.baseframe.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Windows User on 2016/4/14.
+ */
+public class BaseActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
